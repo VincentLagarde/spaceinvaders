560530090d4cbe5f0c9ffe1137e3d19389498bee
Vincent <pataterailgun@gmail.com>
2017-05-22 16:21:49 +0200

    Changement de la vitesse d'un vaisseau

--- spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -1,9 +1,10 @@
 package fr.unilim.iut.spaceinvaders;
 
 public class Vaisseau {
-
-	Position origine;
-	Dimension dimension;
+	
+	private int vitesse;
+	private Position origine;
+	private Dimension dimension;
 
 	public Vaisseau(int longueur, int hauteur) {
 	    this(longueur, hauteur, 0, 0);
@@ -14,9 +15,14 @@ public class Vaisseau {
     }
 
     public Vaisseau(Dimension dimension, Position positionOrigine) {
-	    this.dimension = dimension;
-	    this.origine = positionOrigine;
+    	this(dimension, positionOrigine, 1);
     }
+    
+    public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
+		this.dimension = dimension;
+		this.origine = positionOrigine;
+		this.vitesse = vitesse;
+	}
     
     public boolean occupeLaPosition(int x, int y) {
 			return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
@@ -26,7 +32,7 @@ public class Vaisseau {
 		return (ordonneeLaPlusBasse()<=y) && (y<=ordonneeLaPlusHaute());
 	}
 
-	private int ordonneeLaPlusHaute() {
+	public int ordonneeLaPlusHaute() {
 		return this.origine.ordonnee();
 	}
 
@@ -47,7 +53,7 @@ public class Vaisseau {
 	}
 
 	public void seDeplacerVersLaDroite() {
-		this.origine.changerAbscisse(this.origine.abscisse()+1);
+		this.origine.changerAbscisse(this.origine.abscisse()+vitesse);
 		
 	}
 
@@ -56,7 +62,7 @@ public class Vaisseau {
 	}
 
 	public void seDeplacerVersLaGauche() {
-		this.origine.changerAbscisse(this.origine.abscisse()-1);
+		this.origine.changerAbscisse(this.origine.abscisse()-vitesse);
 		
 	}
 	public void positionner(int x, int y) {
